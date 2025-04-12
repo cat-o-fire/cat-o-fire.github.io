ad961948f20718cdbec00c3fd600f625d2a9957c
cat o fire <anathecurse@gmail.com>
2025-04-12 03:16:47 -0700

    header

---
+++ header.js
@@ -0,0 +1,10 @@
+let appHeader = `
+	<div id ="headGraphics">
+		<img id = "headImageOne" src="technocracyLogo.webp" class="headImage">
+		<img id = "headImageTwo" src="progenitorsLogo.webp" class="headImage">
+		<div id = headText class = "center">
+			<h1>The Dancing Mountain Database</h1>
+		</div>
+	</div>
+`;
+document.getElementById("unionHead").innerHTML = appHeader;
