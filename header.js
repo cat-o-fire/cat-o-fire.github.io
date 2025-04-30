9371756b80e44f2a3f4631002e4e63f81d162cbe
cat o fire <anathecurse@gmail.com>
2025-04-30 02:03:10 -0700

    home link

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ let appHeader = `
 		<img id = "headImageOne" src="technocracyLogo.webp" class="headImage">
 		<img id = "headImageTwo" src="progenitorsLogo.webp" class="headImage">
 		<div id = headText class = "center">
-			<h1>The Dancing Mountain Database</h1>
+			<h1><a href="/UnionHomepage.html">The Dancing Mountain Database</a></h1>
 		</div>
 	</div>
 `;
