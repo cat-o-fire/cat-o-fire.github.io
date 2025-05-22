ab5278c89978075cbcbd2dba8581853f6ba8253c
cat o fire <anathecurse@gmail.com>
2025-05-21 19:22:17 -0700

    .

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 let appHeader = `
 	<div id ="headGraphics">
-		<img id = "headImageOne" src="technocracyLogo.webp" class="headImage">
-		<img id = "headImageTwo" src="progenitorsLogo.webp" class="headImage">
+		<img id = "headImageOne" src="technocracyLogo.webp" class="/headImage">
+		<img id = "headImageTwo" src="progenitorsLogo.webp" class="/headImage">
 		<div id = headText class = "center">
 			<h1><a href="/UnionHomepage.html">The Dancing Mountain Database</a></h1>
 		</div>
