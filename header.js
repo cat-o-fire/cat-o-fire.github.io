0f2e84babce79cef1856fb5401ecca58adca509b
cat o fire <anathecurse@gmail.com>
2026-02-27 05:28:51 -0800

    ?

--- header.js
+++ header.js
@@ -1,10 +1,10 @@
 let appHeader = `
 	<div id ="headGraphics">
-		<img id = "headImageOne" src="technocracyLogo.webp" class="/headImage">
-		<img id = "headImageTwo" src="progenitorsLogo.webp" class="/headImage">
+		<img id = "headImageOne" src="technocracyLogo.webp" class="headImage">
+		<img id = "headImageTwo" src="progenitorsLogo.webp" class="headImage">
 		<div id = headText class = "center">
-			<h1><a href="/UnionHomepage.html">The Dancing Mountain Database</a></h1>
-		</div>
+			<h1><a href="UnionHomepage.html">The Dancing Mountain Database</a></h1>
+		</div>	
 	</div>
 `;
 document.getElementById("unionHead").innerHTML = appHeader;
